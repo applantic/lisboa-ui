3ad0bccfbc97d24ce58cc56c81ea787f3a22e9e4
Daniel Karski <daniel.karski5q@gmail.com>
2018-03-10 16:10:28 +0100

    added router-outlet to core bootstrapped component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
 
 
 import {AppComponent} from './containers/app/app.component';
@@ -13,6 +14,7 @@ export const COMPONENTS = [
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule
   ],
   declarations: COMPONENTS,
   exports: COMPONENTS,
