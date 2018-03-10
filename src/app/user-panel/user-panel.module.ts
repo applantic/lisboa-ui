2c7b4044346e2ce07eaee5af4e0be9d12b34ee1f
Daniel Karski <daniel.karski5q@gmail.com>
2018-03-10 15:37:04 +0100

    generate user-panel.module

---
+++ src/app/user-panel/user-panel.module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+
+
+
+export const COMPONENTS = [
+  
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  declarations: COMPONENTS,
+  exports: COMPONENTS,
+})
+
+export class UserPanelModule {}
