5fd3d0d767aa303115e8c06a8ab31f9bef9ac3f5
Daniel Karski <daniel.karski5q@gmail.com>
2018-03-10 15:59:16 +0100

    generate main-wall-page

---
+++ src/app/main-wall/containers/main-wall-page/main-wall-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'main-wall-page',
+  templateUrl: './main-wall-page.component.html',
+  styleUrls: ['./main-wall-page.component.scss']
+})
+export class MainWallPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/main-wall/main-wall.module.ts
+++ src/app/main-wall/main-wall.module.ts
@@ -3,12 +3,14 @@ import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router/router';
 
 import {OfferResponsePageComponent} from './containers/offer-response-page/offer-response-page.component';
+import {MainWallPageComponent} from './containers/main-wall-page/main-wall-page.component';
 
 
 
 
 export const COMPONENTS = [
   OfferResponsePageComponent,
+  MainWallPageComponent
 ];
 
 @NgModule({
