5c105d4811d4d487ff134fa8380d1b53f9cc1c84
Daniel Karski <daniel.karski5q@gmail.com>
2018-03-10 15:31:34 +0100

    modified structure project to core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
 
+import {CoreModule} from './core/core.module';
 
-import { AppComponent } from './app.component';
-
+import {AppComponent} from './core/containers/app/app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    HttpClientModule,
+    CoreModule.forRoot(),
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+  }
+}
+
