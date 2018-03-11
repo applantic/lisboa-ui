15e48b216e805243842aaf042a0920474366ca7c
Daniel Karski <daniel.karski5q@gmail.com>
2018-03-11 08:43:01 +0100

    generate service Data

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 
-
 import {AppComponent} from './containers/app/app.component';
 import {NotFoundPageComponent} from './containers/not-found-page/not-found-page.component';
+
+import {DataService} from './services/data.service';
 
 export const COMPONENTS = [
   AppComponent,
@@ -24,7 +25,7 @@ export class CoreModule {
   static forRoot() {
     return {
       ngModule: CoreModule,
-      providers: [],
+      providers: [DataService],
     };
   }
 }
