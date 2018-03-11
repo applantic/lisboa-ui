22c521ece293eb2cd054f76571a48a17da394edc
Daniel Karski <daniel.karski5q@gmail.com>
2018-03-11 11:45:01 +0100

    bind my-announcement.json to view state

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {of} from 'rxjs/observable/of';
+import {Observable} from 'rxjs/Rx';
+
+import {MyAnnouncement} from '../../main-wall/model/my-announcement';
 
 @Injectable()
 export class DataService {
 
   constructor() {
     
+  }
+  
+  public getListAnnouncement(): Observable<MyAnnouncement[]>{
+    return of([])
   }
 }
--- src/app/main-wall/services/my-announcement.service.ts
+++ src/app/main-wall/services/my-announcement.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Rx';
+import {MyAnnouncement} from '../model/my-announcement';
+import {DataService} from '../../core/services/data.service';
 
 @Injectable()
 export class MyAnnouncementService {
 
-  constructor() { }
+  constructor(private dataService:DataService) {
+  }
+
+  getListAnnouncement():Observable<MyAnnouncement[]> {
+    return this.dataService.getListAnnouncement()
+      .do((data) => console.log("getListAnnouncement: ", data))
+  }
 
 }
