15fe03aeae42313ebb3f018c9ca885595da21c8d
Daniel Karski <daniel.karski5q@gmail.com>
2018-03-11 07:59:11 +0100

    resolved bug with webpack https://github.com/angular/angular-cli/issues/9825

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,17 +1,21 @@
 import { Routes } from '@angular/router';
 import {NotFoundPageComponent} from './core/containers/not-found-page/not-found-page.component';
 import {AuthGuard} from './auth/services/auth-guard.service';
+import {UserPanelModule} from './user-panel/user-panel.module';
+import {MainWallModule} from './main-wall/main-wall.module';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/main-wall', pathMatch: 'full' },
   {
     path: 'main-wall',
-    loadChildren: './main-wall/main-wall.module#MainWallModule',
+    loadChildren: () => MainWallModule,
+    // loadChildren: './main-wall/main-wall.module#MainWallModule',
     canActivate: [AuthGuard],
   },
   {
     path: 'user-panel',
-    loadChildren: './user-panel/user-panel.module#UserPanelModule',
+    loadChildren: () => UserPanelModule,
+    // loadChildren: './user-panel/user-panel.module#UserPanelModule',
     canActivate: [AuthGuard],
   },
   { path: '**', component: NotFoundPageComponent },
