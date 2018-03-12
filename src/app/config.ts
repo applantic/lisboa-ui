c8cac44082303b0c0191930b795eb67929248cfb
Maciej Matuszewski <maciek.matuszewski@gmail.com>
2018-03-12 22:16:19 +0000

    Add responsive navigation

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,24 +1,26 @@
-import {DeliveryEnum} from './main-wall/model/my-announcement';
+import { DeliveryEnum } from './main-wall/model/my-announcement';
 
-export type DeliveryTypeMap = {[key:string]:string};
+export interface DeliveryTypeMap {
+  [key: string]: string;
+}
 
-export const PERIOD_LIST = ["prepayment", "delivery day", "1", "3", "7", "14", "21", "21+"];
+export const PERIOD_LIST = ['prepayment', 'delivery day', '1', '3', '7', '14', '21', '21+'];
 
-export const DELIVERY_TYPE:DeliveryTypeMap = {
-  [DeliveryEnum.WITH_DELIVERY]: "with delivery",
-  [DeliveryEnum.WITHOUT_DELIVERY]: "without delivery",
-  [DeliveryEnum.BOTH_DELIVERIES]: "both deliveries",
+export const DELIVERY_TYPE: DeliveryTypeMap = {
+  [DeliveryEnum.WITH_DELIVERY]: 'with delivery',
+  [DeliveryEnum.WITHOUT_DELIVERY]: 'without delivery',
+  [DeliveryEnum.BOTH_DELIVERIES]: 'both deliveries',
 };
 
 export interface Product {
-  key:string;
-  name:string;
+  key: string;
+  name: string;
 }
 
 export interface Category {
-  key:string;
-  name:string;
-  products:Product[];
+  key: string;
+  name: string;
+  products: Product[];
 }
 
-export const BASE_URL = 'https://protected-crag-77697.herokuapp.com/v1/'
+export const BASE_URL = 'https://protected-crag-77697.herokuapp.com/v1/';
