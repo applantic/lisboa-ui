321a395c91c68041f4e0da902b720793ac36f87c
Daniel Karski <daniel.karski5q@gmail.com>
2018-03-12 17:00:53 +0100

    update model and changed structure

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,22 +1,24 @@
-export interface DeliveryTypeMap {
-  [key: string]: string;
-}
+import {DeliveryType} from './main-wall/model/my-announcement';
 
-export const PERIOD_LIST = ['prepayment', 'delivery day', '1', '3', '7', '14', '21', '21+'];
+export type DeliveryTypeMap = {[key:string]:string};
 
-export const DELIVERY_TYPE: DeliveryTypeMap = {
-  'WITH_DELIVERY': 'with delivery',
-  'WITHOUT_DELIVERY': 'without delivery',
-  'BOTH_DELIVERIES': 'both deliveries',
+export const PERIOD_LIST = ["prepayment", "delivery day", "1", "3", "7", "14", "21", "21+"];
+
+export const DELIVERY_TYPE:DeliveryTypeMap = {
+  [DeliveryType.WITH_DELIVERY]: "with delivery",
+  [DeliveryType.WITHOUT_DELIVERY]: "without delivery",
+  [DeliveryType.BOTH_DELIVERIES]: "both deliveries",
 };
 
 export interface Product {
-  key: string;
-  name: string;
+  key:string;
+  name:string;
 }
 
 export interface Category {
-  key: string;
-  name: string;
-  products: Product[];
+  key:string;
+  name:string;
+  products:Product[];
 }
+
+export const BASE_URL = 'https://protected-crag-77697.herokuapp.com/v1/'
--- src/app/main-wall/model/my-announcement.ts
+++ src/app/main-wall/model/my-announcement.ts
@@ -1,18 +1,20 @@
 export interface MyAnnouncement {
-  id: string;
-  category: string;
-  createData: string;
-  lastUpdated: string;
-  price?: number;
-  minQuantity?: number;
-  maxQuantity?: number;
-  period?: string;
-  description?: string;
-  delivery: DeliveryType;
-  paymentDate?: string;
-  deliveryDate?: string;
-  deliveryRange?: string;
-  zipCode?: string;
+  id:string;
+  productKey:string;
+  createData:string;
+  lastUpdated:string;
+  price?:number;
+  minQuantity?:number;
+  maxQuantity?:number;
+  period?:string;
+  description?:string;
+  delivery:DeliveryType;
+  paymentDate?:string;
+  deliveryDate?:string;
+  deliveryRange?:string;
+  zipCode?:string;
 }
 
-export type DeliveryType = 'WITH_DELIVERY' | 'WITHOUT_DELIVERY' | 'BOTH_DELIVERIES';
+export enum DeliveryType{
+  WITH_DELIVERY, WITHOUT_DELIVERY, BOTH_DELIVERIES
+}
