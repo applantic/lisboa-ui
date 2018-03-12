74a359861e97dba2301e99e929821a3b6708fcc7
Daniel Karski <daniel.karski5q@gmail.com>
2018-03-12 17:36:54 +0100

    create select-dropdown

---
+++ src/app/main-wall/components/select-dropdown/select-dropdown.component.ts
@@ -0,0 +1,53 @@
+import {Component, forwardRef, Input} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+
+@Component({
+  selector: 'pt-select-dropdown',
+  templateUrl: './select-dropdown.component.html',
+  styleUrls: ['./select-dropdown.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectDropdownComponent),
+      multi: true
+    }
+  ]
+})
+export class SelectDropdownComponent implements ControlValueAccessor {
+
+  constructor() {
+  }
+
+  @Input()
+  choseValue = '';
+
+  get ChoseValue() {
+    return this.choseValue;
+  }
+
+  set ChoseValue(val) {
+    this.choseValue = val;
+    this.propagateChange(this.choseValue);
+  }
+
+  public setNewValue(value:any) {
+    this.ChoseValue = value
+  }
+
+  writeValue(value:any) {
+    if (value !== undefined) {
+      this.choseValue = value;
+    }
+  }
+
+  propagateChange = (_:any) => {
+  };
+
+  registerOnChange(fn) {
+    this.propagateChange = fn;
+  }
+
+  registerOnTouched() {
+  }
+
+}
