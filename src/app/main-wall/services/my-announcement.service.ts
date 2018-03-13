9ba271635271226cf7b7e4881662f6ddb7dbce65
Daniel Karski <daniel.karski5q@gmail.com>
2018-03-13 07:27:44 +0000

    add state with announcement list

--- src/app/main-wall/services/my-announcement.service.ts
+++ src/app/main-wall/services/my-announcement.service.ts
@@ -1,18 +1,25 @@
 import 'rxjs/add/operator/do';
-import {Observable} from 'rxjs/observable';
 import {Injectable} from '@angular/core';
+import {Subject} from 'rxjs/Subject';
+import {Observable} from 'rxjs/observable';
 import {MyAnnouncement} from '../model/my-announcement';
 import {DataService} from '../../core/services/data.service';
 
 @Injectable()
 export class MyAnnouncementService {
+  listAnnouncement: MyAnnouncement[];
+  public listAnnouncementSubject: Subject<MyAnnouncement[]> = new Subject();
 
   constructor(private dataService: DataService) {
   }
 
-  getListAnnouncement(): Observable<MyAnnouncement[]> {
+  getListAnnouncement() {
     return this.dataService.getListAnnouncement(0, 10)
-      .do((data) => console.log('getListAnnouncement: ', data));
+
+      .do((data) => this.listAnnouncement = data)
+      .do((data) => this.listAnnouncementSubject.next(data))
+      .do((data) => console.log('getListAnnouncement: ', data))
+      .subscribe((data) => {});
   }
 
   addNewAnnouncement(myAnnouncement: MyAnnouncement): Observable<MyAnnouncement> {
