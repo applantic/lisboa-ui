6c2b4429618c19446805d505edf044dff6645a49
Daniel Karski <daniel.karski5q@gmail.com>
2018-03-14 14:01:55 +0000

    add redirect to user-panel

--- src/app/user-panel/user-panel.module.ts
+++ src/app/user-panel/user-panel.module.ts
@@ -1,11 +1,27 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {ClarityModule} from '@clr/angular';
 
-export const COMPONENTS = [];
+import {UserPanelPageComponent} from './containers/user-panel-page/user-panel-page.component';
+import {AnnouncementPageComponent} from './containers/announcement-page/announcement-page.component';
+
+export const COMPONENTS = [
+  UserPanelPageComponent,
+  AnnouncementPageComponent
+];
 
 @NgModule({
   imports: [
     CommonModule,
+    ClarityModule,
+    RouterModule.forChild([
+      {path: '', component: UserPanelPageComponent},
+      {
+        path: 'announcement/:id',
+        component: AnnouncementPageComponent
+      },
+    ]),
   ],
   declarations: COMPONENTS,
   exports: COMPONENTS,
