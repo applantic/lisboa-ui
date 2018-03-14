878b16db5169a090e45fe3863417c81593bee545
Daniel Karski <daniel.karski5q@gmail.com>
2018-03-14 12:14:29 +0000

    generate added-announcement-page

---
+++ src/app/main-wall/containers/added-announcement-page/added-announcement-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pt-added-announcement-page',
+  templateUrl: './added-announcement-page.component.html',
+  styleUrls: ['./added-announcement-page.component.scss']
+})
+export class AddedAnnouncementPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
