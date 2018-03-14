82fc5400b4d5a5bfa4a50a6b21e77deed333de69
Daniel Karski <daniel.karski5q@gmail.com>
2018-03-14 13:27:33 +0000

    generate user-panel-page in user-panel.module

---
+++ src/app/user-panel/containers/user-panel-page/user-panel-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pt-user-panel-page',
+  templateUrl: './user-panel-page.component.html',
+  styleUrls: ['./user-panel-page.component.scss']
+})
+export class UserPanelPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
